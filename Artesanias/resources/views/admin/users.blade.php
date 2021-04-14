727dbd121e7f32e28d79b7869147e14a78d8e222
Marisol Realivazquez <66392967+Uinose@users.noreply.github.com>
2021-04-13 23:00:31 -0600

    Sistemas de plantillas

---
+++ Artesanias/resources/views/admin/users.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layouts.main')
+
+@section('contenido')
+    <h2>Estoy en el admin</h2>
+@endsection
+@section('scripts')
+    <script> console.log("asdfg");</script>
+@endsection
--- Artesanias/routes/web.php
+++ Artesanias/routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {return view('index');});
+Route::get('/admin', function () {return view('admin.index');});
+Route::get('/admin/usuarios', function () {return view('admin.users');});
+Route::get('/admin/productos', function () {return view('admin.productos');});
 Route::get('/producto/{id}/{nombre}', function ($id,$nombre) {
     return view('verproducto')->with('id',$id)->with('nombre',$nombre);
 });
